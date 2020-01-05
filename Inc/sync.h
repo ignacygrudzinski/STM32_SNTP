704e2b10b5698b35e3dbb38dcfdcab0d96d164b8
ignacygrudzinski <ignacy.grudzinski@gmail.com>
2020-01-05 16:51:38 +0100

    Add basic time sync

--- Inc/sync.h
+++ Inc/sync.h
@@ -9,5 +9,26 @@ extern SemaphoreHandle_t connectionEnableSemaphore;
 
 void SY_TaskFunc(void* param);
 
+typedef struct {
+        uint32_t seconds;
+        uint32_t seconds_fraction;
+} SNTP_Timestamp;
+
+typedef struct {
+        uint8_t header;
+        uint8_t stratum;
+        uint8_t poll;
+        uint8_t precision;
+        uint32_t root_delay;
+        uint32_t root_dispersion;
+        uint32_t reference_identifier;
+        SNTP_Timestamp reference_timestamp;
+        SNTP_Timestamp originate_timestamp;
+        SNTP_Timestamp receive_timestamp;
+        SNTP_Timestamp transit_timestamp;
+} SNTP_Frame;
+
+RTC_DateTypeDef SNTP_TimestampToDate(SNTP_Timestamp timestamp);
+RTC_TimeTypeDef SNTP_TimestampToTime(SNTP_Timestamp timestamp);
 
 #endif
