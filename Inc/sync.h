0647e1960564b7e549d9571d8b533637d3782f3a
ignacygrudzinski <ignacy.grudzinski@gmail.com>
2020-01-06 20:27:07 +0100

    xd

--- Inc/sync.h
+++ Inc/sync.h
@@ -5,6 +5,13 @@
 
 #define SY_TASK "SYNC_TASK"
 
+#define DEBUG
+#ifdef DEBUG
+        #define debug_only(command) command;
+#else
+        #define debug_only(command)
+#endif
+
 extern SemaphoreHandle_t connectionEnableSemaphore;
 extern SemaphoreHandle_t syncSemaphore;
 
@@ -29,7 +36,22 @@ typedef struct {
         SNTP_Timestamp transit_timestamp;
 } SNTP_Frame;
 
+#define SNTP_MODE_CLIENT 3
+#define SNTP_MODE_SERVER 4
+
+#define SNTP_UNIX_TIMESTAMP_DIFF 2208990300
+
+#define SNTP_SECONDS_FRACTION_SHIFT 8
+#define SUBSECONDS_PER_SECOND (2 << SNTP_SECONDS_FRACTION_SHIFT)
+
+int Connect(struct netconn **conn);
 void SNTP_TimestampToTime(SNTP_Timestamp *timestamp, RTC_TimeTypeDef *rtc_time);
 void SNTP_TimestampToDate(SNTP_Timestamp *timestamp, RTC_DateTypeDef *rtc_date);
+void LocalDateTimeToSNTP(SNTP_Timestamp *sntp_time, RTC_DateTypeDef *rtc_date, RTC_TimeTypeDef *rtc_time);
+int SNTP_GetTime(SNTP_Timestamp *currentTimestamp, struct netconn *conn);
+void SetRTCTime(SNTP_Timestamp *timestamp);
+void SetRemoteTime(struct netconn *conn);
+void PrintCurrentTime();
+void Sync(struct netconn *conn);
 
 #endif
